bc349c2723c0d7e230b85152a38dfa3231830468
William A. Kennington III <wak@google.com>
2021-07-30 12:10:25 -0700

    sel_logger: Annotate unused variables
    Otherwise some optionsets will produce warnings about unused variables.
    
    Change-Id: I9b223359a33c3e03d63dccedc38dc1b40182f025
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/sel_logger.cpp
+++ src/sel_logger.cpp
@@ -316,10 +316,11 @@ static void writeSELEvent(const unsigned int& recordId, const std::string& selDa
 }
 
 template <typename... T>
-static uint16_t
-    selAddSystemRecord(const std::string& message, const std::string& path,
-                       const std::vector<uint8_t>& selData, const bool& assert,
-                       const uint16_t& genId, T&&... metadata)
+static uint16_t selAddSystemRecord([[maybe_unused]] const std::string& message,
+                                   const std::string& path,
+                                   const std::vector<uint8_t>& selData,
+                                   const bool& assert, const uint16_t& genId,
+                                   [[maybe_unused]] T&&... metadata)
 {
     // Only 3 bytes of SEL event data are allowed in a system record
     if (selData.size() > selEvtDataMaxSize)
@@ -365,7 +366,7 @@ static uint16_t
 #endif
 }
 
-static uint16_t selAddOemRecord(const std::string& message,
+static uint16_t selAddOemRecord([[maybe_unused]] const std::string& message,
                                 const std::vector<uint8_t>& selData,
                                 const uint8_t& recordType)
 {
@@ -394,7 +395,7 @@ static uint16_t selAddOemRecord(const std::string& message,
 #endif
 }
 
-int main(int argc, char* argv[])
+int main(int, char*[])
 {
     // setup connection to dbus
     boost::asio::io_service io;
